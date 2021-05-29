5d20baa5c5458b6f9707d05be6b9d5d393685da5
Avishek Sen Gupta <avishek.sen.gupta@gmail.com>
2021-05-29 17:56:29 +0530

    Mostly done cleaning data

--- investment-assignment/code/test.py
+++ investment-assignment/code/test.py
@@ -51,29 +51,38 @@ def analyse():
     #Fix inconsistent Data
     # Fix BORéAL-BIKES-INCORPORATED
 
-    fix("Boréal Bikes Incorporated", "/organization/boréal-bikes-incorporated", rounds2, companies)
-    fix("Tío Conejo", "/organization/tío-conejo", rounds2, companies)
-    fix("Monnier Frères", "/organization/monnier-frères", rounds2, companies)
-    fix("Affluent Attaché Club", "/organization/affluent-attaché-club-2", rounds2, companies)
-    fix("Jean Pütz Produkte", "/organization/jean-pütz-produkte", rounds2, companies)
-    fix("PatroFİN", "/organization/patrofi̇n", rounds2, companies)
-    fix("Salão VIP", "/organization/salão-vip", rounds2, companies)
-    fix("Proděti.cz", "/organization/proděti-cz", rounds2, companies)
-    fix("LawPadi", "/organization/lawpàdí", rounds2, companies)
-    fix("eTool.io", "/organization/etool-io", rounds2, companies)
-    fix("Crème & Ciseaux", "/organization/crème-ciseaux", rounds2, companies)
-    fix("Prześwietl.pl", "/organization/prześwietl-pl", rounds2, companies)
-    fix("Capptú", "/organization/capptú", rounds2, companies)
-    fix("Gráfica en línea", "/organization/gráfica-en-línea", rounds2, companies)
-    fix("IGNIA Bienes Raíces", "/organization/ignia-bienes-raíces", rounds2, companies)
-    fix("Bricoprivé.com", "/organization/bricoprivé-com", rounds2, companies)
-    fix("Médica Santa Carmen", "/organization/médica-santa-carmen-2", rounds2, companies)
-    fix("Weiche Tech 喂车科技", "/organization/weiche-tech-喂车科暀", rounds2, companies)
-    # fix("Magnet Tech 磁石科技", "/organization/magnet-tech-磁??科暀", rounds2, companies)
+    fix_permalink_from_rounds("Boréal Bikes Incorporated", "/organization/boréal-bikes-incorporated", companies,
+                              rounds2)
+    fix_permalink_from_rounds("Tío Conejo", "/organization/tío-conejo", companies, rounds2)
+    fix_permalink_from_rounds("Monnier Frères", "/organization/monnier-frères", companies, rounds2)
+    fix_permalink_from_rounds("Affluent Attaché Club", "/organization/affluent-attaché-club-2", companies, rounds2)
+    fix_permalink_from_rounds("Jean Pütz Produkte", "/organization/jean-pütz-produkte", companies, rounds2)
+    fix_permalink_from_rounds("PatroFİN", "/organization/patrofi̇n", companies, rounds2)
+    fix_permalink_from_rounds("Salão VIP", "/organization/salão-vip", companies, rounds2)
+    fix_permalink_from_rounds("Proděti.cz", "/organization/proděti-cz", companies, rounds2)
+    fix_permalink_from_rounds("LawPadi", "/organization/lawpàdí", companies, rounds2)
+    fix_permalink_from_rounds("eTool.io", "/organization/etool-io", companies, rounds2)
+    fix_permalink_from_rounds("Crème & Ciseaux", "/organization/crème-ciseaux", companies, rounds2)
+    fix_permalink_from_rounds("Prześwietl.pl", "/organization/prześwietl-pl", companies, rounds2)
+    fix_permalink_from_rounds("Capptú", "/organization/capptú", companies, rounds2)
+    fix_permalink_from_rounds("Gráfica en línea", "/organization/gráfica-en-línea", companies, rounds2)
+    fix_permalink_from_rounds("IGNIA Bienes Raíces", "/organization/ignia-bienes-raíces", companies, rounds2)
+    fix_permalink_from_rounds("Bricoprivé.com", "/organization/bricoprivé-com", companies, rounds2)
+    fix_permalink_from_rounds("Médica Santa Carmen", "/organization/médica-santa-carmen-2", companies, rounds2)
+    fix_permalink_from_rounds("E CÚBICA", "/organization/e-cêbica", companies, rounds2)
+    fix_permalink_from_rounds("Vá de Táxi", "/organization/vá-de-táxi", companies, rounds2)
+
+    fix_permalink_from_companies("It’s All About Me", "S-ALL-ABOUT-ME", companies, rounds2)
     # fix("iProof - The Foundation for the Internet of Things", "/organization/affluent-attaché-club-2", rounds2, companies)
     fix_zengame(companies, rounds2, "ZenGame", "ZenGame 禅游科技")
     fix_zengame(companies, rounds2, "EnergyStone Games", "EnergyStone Games 灵石游戏")
     fix_zengame(companies, rounds2, "Magnet Tech ", "Magnet Tech 磁石科技")
+    fix_zengame(companies, rounds2, "Huizuche.com", "Huizuche.com 惠租车")
+    fix_zengame(companies, rounds2, "Inveno ", "Inveno 英威诺")
+    fix_zengame(companies, rounds2, "Weiche Tech ", "Weiche Tech 喂车科技")
+    fix_zengame(companies, rounds2, "TipCat Interactive", "TipCat Interactive 沙舟信息科技")
+    fix_zengame(companies, rounds2, "Jiwu", "Jiwu 吉屋网")
+    fix_zengame(companies, rounds2, "TalentSigned", "TalentSigned™")
 
     x, y = uniques(companies, rounds2)
     print(set(y).difference(set(x)))
@@ -86,25 +95,35 @@ def fix_zengame(companies, rounds2, company_name_prefix, full_company_name):
     correct_name = zengame.iloc[0][COMPANIES_NAME]
     # result = re.compile(f"{original_company_name_prefix} (.*)").search(correct_name)
     dashed_company_name_prefix = company_name_prefix.replace(' ', '-')
-    dashed_lowercase_full_company_name = full_company_name.replace(' ', '-').lower()
+    dashed_lowercase_full_company_name = full_company_name.replace(' ', '-').replace('.', ' ').lower()
     corrected_permalink_lowercase = (f'/organization/{dashed_lowercase_full_company_name}').lower()
     print(corrected_permalink_lowercase)
     companies.loc[companies[COMPANIES_NAME].str.contains(full_company_name, na=False), COMPANIES_COMPANY_PERMALINK_LOWERCASE] = corrected_permalink_lowercase
-    rounds2.loc[rounds2[ROUNDS2_COMPANY_PERMALINK].str.contains(dashed_company_name_prefix,
+    rounds2.loc[rounds2[ROUNDS2_COMPANY_PERMALINK].str.contains(f'/organization/{dashed_company_name_prefix}',
                                                                 na=False, case=False), ROUNDS2_COMPANY_PERMALINK_LOWERCASE] = corrected_permalink_lowercase
     # print(companies.iloc[65778])
     print(companies[companies[COMPANIES_COMPANY_PERMALINK_LOWERCASE].str.contains(corrected_permalink_lowercase, na=False, case=False)])
     print(rounds2[rounds2[ROUNDS2_COMPANY_PERMALINK_LOWERCASE].str.contains(corrected_permalink_lowercase, na=False, case=False)])
 
-def fix(company_name, company_permalink, truth, corrupted):
+def fix_permalink_from_rounds(company_name, company_permalink, companies, rounds):
     global COMPANIES_NAME
 
-    correct_value_rows = truth[truth[ROUNDS2_COMPANY_PERMALINK_LOWERCASE] == company_permalink]
+    correct_value_rows = rounds[rounds[ROUNDS2_COMPANY_PERMALINK_LOWERCASE] == company_permalink]
     # print(correct_value_rows.iloc[0][ROUNDS2_COMPANY_PERMALINK_LOWERCASE])
     corrected_value = correct_value_rows.iloc[0][ROUNDS2_COMPANY_PERMALINK_LOWERCASE]
-    corrupted.loc[corrupted[COMPANIES_NAME] == company_name, COMPANIES_COMPANY_PERMALINK_LOWERCASE] = corrected_value
-    print(corrupted[corrupted[COMPANIES_NAME] == company_name].to_string())
-    print(truth[truth[ROUNDS2_COMPANY_PERMALINK_LOWERCASE] == corrected_value].to_string())
+    companies.loc[companies[COMPANIES_NAME] == company_name, COMPANIES_COMPANY_PERMALINK_LOWERCASE] = corrected_value
+    print(companies[companies[COMPANIES_NAME] == company_name].to_string())
+    print(rounds[rounds[ROUNDS2_COMPANY_PERMALINK_LOWERCASE] == corrected_value].to_string())
+
+def fix_permalink_from_companies(company_name, rounds_permalink_fragment, companies, rounds):
+    global COMPANIES_NAME
+
+    correct_value_rows = companies[companies[COMPANIES_NAME] == company_name]
+    # print(correct_value_rows.iloc[0][ROUNDS2_COMPANY_PERMALINK_LOWERCASE])
+    lowercase_corrected_value = correct_value_rows.iloc[0][COMPANIES_COMPANY_PERMALINK_LOWERCASE].lower()
+    rounds.loc[rounds[ROUNDS2_COMPANY_PERMALINK_LOWERCASE].str.contains(rounds_permalink_fragment, na=False, case=False), ROUNDS2_COMPANY_PERMALINK_LOWERCASE] = lowercase_corrected_value
+    print(companies[companies[COMPANIES_COMPANY_PERMALINK_LOWERCASE] == lowercase_corrected_value].to_string())
+    print(rounds[rounds[ROUNDS2_COMPANY_PERMALINK_LOWERCASE] == lowercase_corrected_value].to_string())
 
 def uniques(companies, rounds2):
     global ROUNDS2_COMPANY_PERMALINK_LOWERCASE
