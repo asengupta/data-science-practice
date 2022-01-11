c9aed7e50a9c9a9e3feb6b215f7c07743a7f4566
Avishek Sen Gupta <avishek.sen.gupta@gmail.com>
2022-01-11 12:05:42 +0530

    Initial script

--- nlp-assignment/code/assignment.py
+++ nlp-assignment/code/assignment.py
@@ -35,6 +35,15 @@ def print_first_5(preprocessed_lines):
     logging.info(list(first_5_sentences))
 
 
+def pos_tagged(sentences):
+    # print(f"Sentences are: {sentences}")
+    return list(map(lambda sentence_arr: model(" ".join(sentence_arr)), sentences))
+
+
+def nouns(doc):
+    return list(filter(lambda token: token.pos_ == "NOUN" or token.pos_ == "PROPN", doc))
+
+
 def analyse():
     preprocessed_train_sentences = preprocessed(DATA_TRAIN_SENTENCES)
     preprocessed_train_labels = preprocessed(DATA_TRAIN_LABELS)
@@ -48,6 +57,20 @@ def analyse():
     logging.info(f"NUMBER OF SENTENCES IN TESTING SET = {len(preprocessed_test_sentences)}")
     logging.info(f"NUMBER OF LABEL LINES IN TRAINING SET = {len(preprocessed_train_labels)}")
     logging.info(f"NUMBER OF LABEL LINES IN TESTING SET = {len(preprocessed_test_labels)}")
+
+    tagged_documents = pos_tagged(preprocessed_train_sentences + preprocessed_test_sentences)
+    all_noun_tokens = reduce(lambda acc, doc: acc + nouns(doc), tagged_documents, [])
+
+    dict = {}
+    for noun_token in all_noun_tokens:
+        if noun_token.lemma_ in dict.keys():
+            dict[noun_token.lemma_] = dict[noun_token.lemma_] + 1
+        else:
+            dict[noun_token.lemma_] = 1
+
+    noun_occurrences = list(dict.items())
+    noun_occurrences.sort(reverse=True, key=lambda e: e[1])
+    logging.info(f"25 Most Common Nouns Used: {noun_occurrences[:26]}")
 
 
 def setup_logging():
